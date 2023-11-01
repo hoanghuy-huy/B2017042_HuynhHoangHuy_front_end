fb09228588f7ac25f99d6938446f8e7ba66ac0ab
hoanghuy-huy <huynhhoanghuy221122@gmail.com>
2023-11-01 08:56:47 +0700

    update

--- src/views/ContactAdd.vue
+++ src/views/ContactAdd.vue
@@ -5,36 +5,36 @@
       />
       <h1></h1>
     </div>
-  </template>
+</template>
   
-  <script>
-  import ContactForm from "@/components/ContactForm.vue";
-  import ContactService from "@/services/contact.service";
-  
-  export default {
-    components: {
-      ContactForm,
-    },
-    data() {
-      return {
-        contactData: {
-          name: "",
-          email: "",
-          address: "",
-          phone: "",
-          favorite: false,
-        },
-      };
-    },
-    methods:{
-        async addContact(data) {
-        try {
-            await ContactService.create(data)
-            confirm('Đã thêm liên hệ mới')
-        } catch (error) {
-          console.log(error);
-        }
+<script>
+import ContactForm from "@/components/ContactForm.vue";
+import ContactService from "@/services/contact.service";
+
+export default {
+  components: {
+    ContactForm,
+  },
+  data() {
+    return {
+      contactData: {
+        name: "",
+        email: "",
+        address: "",
+        phone: "",
+        favorite: false,
       },
-    }
-  };
-  </script>
+    };
+  },
+  methods:{
+      async addContact(data) {
+      try {
+          await ContactService.create(data)
+          confirm('Đã thêm liên hệ mới')
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  }
+};
+</script>
